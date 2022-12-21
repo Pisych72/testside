4c3f87f98d5d49d7e28296b4109ef9bc28487b77
Dmitry Shaporev <dms72@list.ru>
2022-12-21 14:17:54 +0700

    Forms. Begin.

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_list_or_404
 from django.shortcuts import HttpResponse
 from .models import *
+from .forms import NewsForm
 # Create your views here.
 def index(request):
 
@@ -21,3 +22,10 @@ def view_news(request,news_id):
    # news_item=news.objects.get(pk=news_id)
     news_item=get_list_or_404(news,pk=news_id)
     return render(request,'news/view_news.html',{'news_item':news_item})
+
+def add_news(request):
+    if request.method=='POST':
+        pass
+    else:
+        form=NewsForm()
+    return render(request,'news/add_news.html',{'form': form} )
